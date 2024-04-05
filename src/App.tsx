779a41351825f01a14c1fe7cac1c8ee57e3f1a60
Roger Perez <ropere@microsoft.com>
2024-04-05 12:54:24 -0700

    app fetch calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Book, IBook } from "./Book";
+
+interface IBookList {
+  [key: string]: IBook[];
+}
 
 function App() {
+  const [books, setBooks] = useState<IBookList>();
+
+  useEffect(() => {
+    fetch("./book.json")
+      .then((response) => response.json())
+      .then((json) => {
+        setBooks(json);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {books &&
+        Object.entries(books).map((bookData: any) => {
+          console.log("BOOL:", bookData);
+          const [letter, bookArr] = bookData;
+          console.log("L:", letter, bookArr);
+          return (
+            <div>
+              <div>{letter}</div>
+              {bookArr.map((data: IBook) => {
+                return <Book key={data.id} {...data} />;
+              })}
+            </div>
+          );
+        })}
     </div>
   );
 }
