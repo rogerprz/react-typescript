e6a828b6ccffa0b0c39c7475177324f08acfb913
Roger Perez <ropere@microsoft.com>
2024-04-06 11:00:42 -0700

    add esling

---
+++ src/BookForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const BookForm = (): JSX.Element => {
+  return (
+    <form>
+      <textarea name="first" id="" cols={30} rows={10}></textarea>
+    </form>
+  );
+};
